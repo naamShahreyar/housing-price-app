ec999021f82c5848c058b7ee808d5e5aa9334010
naamShahreyar <shahreyar1411@gmail.com>
2026-04-14 21:35:22 +0530

    Add: data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,26 @@
+from src.constants.config import config
+from src.logger import get_logger
+from sklearn.datasets import fetch_california_housing
+import os
+import pandas as pd
+
+logger = get_logger(__name__)
+
+class DataIngestion:
+
+    def __init__(self):
+        self.raw_path = config["artifacts"]["raw_data"]
+
+    def run(self):
+        logger.info("Starting data ingestion")
+
+        os.makedirs(os.path.dirname(self.raw_path), exist_ok=True)
+
+        data = fetch_california_housing(as_frame=True)
+        df = pd.concat([data.data, data.target.rename("target")], axis=1)
+
+        df.to_csv(self.raw_path, index=False)
+
+        logger.info(f"Data saved to {self.raw_path}")
+
+        return self.raw_path
