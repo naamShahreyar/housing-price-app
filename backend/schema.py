92d25c8bebd9bc06e3f99170f05c90ca1c73e13c
naamShahreyar <shahreyar1411@gmail.com>
2026-04-15 09:59:58 +0530

    Add: Schema.py

---
+++ backend/schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class HouseInput(BaseModel):
+    MedInc: float
+    HouseAge: float
+    AveRooms: float
+    AveBedrms: float
+    Population: float
+    AveOccup: float
+    Latitude: float
+    Longitude: float
+
+
+class PredictionOutput(BaseModel):
+    prediction: float
