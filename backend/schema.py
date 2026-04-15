5ea106c9f41a080abfca5962654d2642be32155e
naamShahreyar <shahreyar1411@gmail.com>
2026-04-15 10:10:23 +0530

    Update: schema.py

--- backend/schema.py
+++ backend/schema.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 
 class HouseInput(BaseModel):
-    MedInc: float
-    HouseAge: float
-    AveRooms: float
-    AveBedrms: float
-    Population: float
-    AveOccup: float
-    Latitude: float
-    Longitude: float
+    MedInc: Annotated[float, Field(..., description="Median income in block group", examples=[3.5])]
+    HouseAge: Annotated[float, Field(..., description="Age of house", examples=[25.0])]
+    AveRooms: Annotated[float, Field(..., description="Average number of rooms per household", examples=[5.0])]
+    AveBedrms: Annotated[float, Field(..., description="Average number of bedrooms per household", examples=[1.0])]
+    Population: Annotated[float, Field(..., description="Population in block group", examples=[1000.0])]
+    AveOccup: Annotated[float, Field(..., description="Average number of household members", examples=[3.0])]
+    Latitude: Annotated[float, Field(..., description="Latitude of block group", examples=[37.88])]
+    Longitude: Annotated[float, Field(..., description="Longitude of block group", examples=[-122.23])]
 
 
 class PredictionOutput(BaseModel):
